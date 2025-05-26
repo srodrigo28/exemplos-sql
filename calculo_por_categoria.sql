cef92ec4610a6cd12c72605a47c90e020cf6af7a
SEBASTIAO RODRIGO NASCIMENTO SOUSA <rodrigoexer1@gmail.com>
2025-05-26 13:04:07 -0300

    Create calculo_por_categoria.sql

---
+++ calculo_por_categoria.sql
@@ -0,0 +1,26 @@
+create or replace view view_rendimento_mes as
+select
+  sa.user_ref,
+  sa.saldo_atual,
+  p.percentual,
+  sa.saldo_atual * p.percentual as rendimento,
+  sum(
+    case
+      when l.categoria = 'Aplicado' then l.valor
+      else 0
+    end
+  ) as soma_aplicado,
+  sum(
+    case
+      when l.categoria = 'Retirada' then l.valor
+      else 0
+    end
+  ) as soma_retiradas
+from
+  saldo_atual sa
+  join perfil p on sa.user_ref = p.user_ref
+  left join lancamento l on sa.user_ref = l.user_ref
+group by
+  sa.user_ref,
+  sa.saldo_atual,
+  p.percentual;
