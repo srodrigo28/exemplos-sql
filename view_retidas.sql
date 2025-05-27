d43f531d3a31dad4845e019656ebd6008818c4cd
SEBASTIAO RODRIGO NASCIMENTO SOUSA <rodrigoexer1@gmail.com>
2025-05-27 14:22:57 -0300

    Update view_retidas.sql

--- view_retidas.sql
+++ view_retidas.sql
@@ -1,14 +1,14 @@
-create or replace view view_retidas as
+drop view if exists view_retidas;
+
+create view view_retidas as
 select
   l.user_ref,
   l.categoria,
-  sum(l.valor) as valor,
-  l.data
+  sum(l.valor) as valor
 from
   lancamento l
 where
   l.categoria = 'Retirada'
 group by
   l.user_ref,
-  l.categoria,
-  l.data;
+  l.categoria;
