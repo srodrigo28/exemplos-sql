51fd94f75acd710100a321a6eb6125eb82e4a394
SEBASTIAO RODRIGO NASCIMENTO SOUSA <rodrigoexer1@gmail.com>
2025-06-17 11:07:00 -0300

    Create calculo_horas.sql

---
+++ calculo_horas.sql
@@ -0,0 +1,27 @@
+create table app_data (
+  id bigint primary key generated always as identity,
+  created_at timestamp with time zone default now(),
+  nome text,
+  horario_inicio time,
+  horario_saiu time,
+  hora_total interval,
+  hora_extra interval
+);
+
+create
+or replace function update_hours () returns trigger as $$
+BEGIN
+    NEW.hora_total := NEW.horario_saiu - NEW.horario_inicio;
+    IF NEW.hora_total > interval '8 hours' THEN
+        NEW.hora_extra := NEW.hora_total - interval '8 hours';
+    ELSE
+        NEW.hora_extra := interval '0';
+    END IF;
+    RETURN NEW;
+END;
+$$ language plpgsql;
+
+create trigger calculate_hours before insert
+or
+update on app_data for each row
+execute function update_hours ();
