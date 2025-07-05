3058ee074b15a4ac40bf5aa68d940c9aa52e9849
SEBASTIAO RODRIGO NASCIMENTO SOUSA <rodrigoexer1@gmail.com>
2025-07-05 11:59:57 -0300

    Create conta-pessoal.sql

---
+++ conta-pessoal.sql
@@ -0,0 +1,42 @@
+create table usuario (
+  id bigint primary key generated always as identity,
+  nome text not null,
+  tipo text not null,
+  email text not null,
+  telefone text not null,
+  user_ref uuid not null unique,
+  saldo_inicial numeric(15, 2) not null,
+  created_at timestamp with time zone default now() not null,
+  updated_at timestamp with time zone default now() not null
+);
+
+create table categorias (
+  id bigint primary key generated always as identity,
+  nome text not null,
+  descricao text not null,
+  created_at timestamp with time zone default now() not null,
+  updated_at timestamp with time zone default now() not null
+);
+
+create table transacoes (
+  id bigint primary key generated always as identity,
+  data date not null,
+  valor numeric(15, 2) not null,
+  tipo text check (tipo in ('entrada', 'saida')) not null,
+  conta_id bigint references usuario (id),
+  categoria_id bigint references categorias (id),
+  descricao text,
+  created_at timestamp with time zone default now() not null,
+  updated_at timestamp with time zone default now() not null
+);
+
+create table lancamento_usuario_futuras (
+  id bigint primary key generated always as identity,
+  data_prevista date not null,
+  valor numeric(15, 2) not null,
+  tipo text check (tipo in ('entrada', 'saida')) not null,
+  conta_id bigint references usuario (id),
+  descricao text,
+  created_at timestamp with time zone default now() not null,
+  updated_at timestamp with time zone default now() not null
+);
